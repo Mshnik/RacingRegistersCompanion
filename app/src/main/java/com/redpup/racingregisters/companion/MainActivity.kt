a130ffbc2160e5872938072eb16955be791a5bfa
michaelpatashnik <mgpshnik@gmail.com>
2025-02-10 22:53:48 -0500

    More stuff from tutotial

--- app/src/main/java/com/redpup/racingregisters/companion/MainActivity.kt
+++ app/src/main/java/com/redpup/racingregisters/companion/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.redpup.racingregisters.companion
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.redpup.racingregisters.companion.ui.theme.RacingRegistersCompanionTheme
 
@@ -43,22 +48,46 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun Greeting(message: Message, modifier: Modifier = Modifier) {
-  Surface(color = Color.Black) {
+  Row(
+    modifier = modifier
+      .padding(all = 8.dp)
+      .border(1.5.dp, MaterialTheme.colorScheme.primary)
+  ) {
+    Image(
+      painter = painterResource(R.drawable.circuit),
+      contentDescription = "Circuit Image",
+      modifier = modifier
+        .size(40.dp)
+    )
     Column {
-      Image(
-        painter = painterResource(R.drawable.circuit),
-        contentDescription = "Circuit Image",
-        modifier = modifier.size(40.dp)
-      )
       Text(
         text = "Hi, my name is ${message.author}!",
         modifier = modifier.padding(24.dp),
       )
       Spacer(modifier = modifier.width(8.dp))
-      Text(
-        text = "My message is ${message.body}!",
-        modifier = modifier.padding(24.dp)
-      )
+      Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+        Text(
+          text = "My message is ${message.body}!",
+          color = MaterialTheme.colorScheme.secondary,
+          style = MaterialTheme.typography.bodySmall,
+          modifier = modifier.padding(24.dp)
+        )
+      }
+    }
+  }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+  uiMode = Configuration.UI_MODE_NIGHT_YES,
+  showBackground = true,
+  name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+  RacingRegistersCompanionTheme {
+    Surface {
+      Greeting(message = Message("Michael", "Hello"))
     }
   }
 }
