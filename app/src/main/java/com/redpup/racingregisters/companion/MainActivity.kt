69b88a3135b988e996e4e359c6bf044c5a5966b7
michaelpatashnik <mgpshnik@gmail.com>
2025-02-10 22:44:53 -0500

    First part of composition tutorial in https://developer.android.com/develop/ui/compose/tutorial

--- app/src/main/java/com/redpup/racingregisters/companion/MainActivity.kt
+++ app/src/main/java/com/redpup/racingregisters/companion/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.redpup.racingregisters.companion.ui.theme.RacingRegistersCompanionTheme
 
@@ -24,7 +30,7 @@ class MainActivity : ComponentActivity() {
       RacingRegistersCompanionTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
           Greeting(
-            name = "Android",
+            message = Message("Michael", "Hello"),
             modifier = Modifier.padding(innerPadding)
           )
         }
@@ -33,20 +39,26 @@ class MainActivity : ComponentActivity() {
   }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Surface(color = Color.Cyan) {
-    Text(
-      text = "Hi, my name is $name!",
-      modifier = modifier.padding(24.dp)
-    )
-  }
-}
+data class Message(val author: String, val body: String)
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-  RacingRegistersCompanionTheme {
-    Greeting("Android")
+fun Greeting(message: Message, modifier: Modifier = Modifier) {
+  Surface(color = Color.Black) {
+    Column {
+      Image(
+        painter = painterResource(R.drawable.circuit),
+        contentDescription = "Circuit Image",
+        modifier = modifier.size(40.dp)
+      )
+      Text(
+        text = "Hi, my name is ${message.author}!",
+        modifier = modifier.padding(24.dp),
+      )
+      Spacer(modifier = modifier.width(8.dp))
+      Text(
+        text = "My message is ${message.body}!",
+        modifier = modifier.padding(24.dp)
+      )
+    }
   }
 }
