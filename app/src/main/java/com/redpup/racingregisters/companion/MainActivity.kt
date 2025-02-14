e3541da22dd2942ef6a88cb0789885f4fce377f9
michaelpatashnik <mgpshnik@gmail.com>
2025-02-14 13:41:58 -0500

    Update button styling by state.

--- app/src/main/java/com/redpup/racingregisters/companion/MainActivity.kt
+++ app/src/main/java/com/redpup/racingregisters/companion/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -21,6 +22,7 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -32,6 +34,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -39,7 +42,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.redpup.racingregisters.companion.timer.Timer
+import com.redpup.racingregisters.companion.ui.theme.Green90
 import com.redpup.racingregisters.companion.ui.theme.RacingRegistersCompanionTheme
+import com.redpup.racingregisters.companion.ui.theme.White90
 import com.redpup.racingregisters.companion.ui.theme.sixtyFour
 
 class MainActivity : ComponentActivity() {
@@ -62,15 +67,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RenderScreen(timer: Timer, modifier: Modifier = Modifier) {
   Column(
-    modifier = Modifier.fillMaxSize()
-      .padding(10.dp)
-      // .background(androidx.compose.ui.graphics.Color.Red, shape = RectangleShape)
-    ,
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(10.dp),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally
   ) {
     RenderedTimer(
-      timer = timer, modifier = modifier
+      timer = timer, modifier = modifier.height(100.dp)
     )
     RenderBreakContinueButton(timer)
   }
@@ -80,19 +84,20 @@ fun RenderScreen(timer: Timer, modifier: Modifier = Modifier) {
 fun RenderedTimer(timer: Timer, modifier: Modifier = Modifier) {
   Box(
     modifier = modifier
-      .padding(all = 12.dp)
-      .border(1.5.dp, MaterialTheme.colorScheme.primary)
+      .border(5.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(2.dp))
+      .background(Color.Blue)
   ) {
     Box(
-      modifier = modifier.padding(all = 12.dp)
+      modifier = modifier
+        .background(Color.Red)
     ) {
       val currentTime = remember { mutableStateOf(timer.toString()) }
       val timerFont = TextStyle(
         fontFamily = sixtyFour,
         fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.sp
+        fontSize = 50.sp,
+        lineHeight = 0.sp,
+        letterSpacing = 4.sp
       )
       timer.subscribe { currentTime.value = timer.toString() }
       Text(text = currentTime.value, style = timerFont)
@@ -106,15 +111,41 @@ fun RenderBreakContinueButton(
   modifier: Modifier = Modifier,
   initialText: String = "CONTINUE",
 ) {
-  var buttonText by remember { mutableStateOf(initialText) }
+  var text by remember { mutableStateOf(initialText) }
+  var textColor by remember { mutableStateOf(Color.Black) }
+  var backgroundColor by remember { mutableStateOf(Color.Black) }
+  var borderColor by remember { mutableStateOf(Color.Black) }
+
+  fun updateColors() {
+    textColor = if (text == "BREAK") Color.Black else Green90
+    backgroundColor = if (text == "BREAK") White90 else Color.Black
+    borderColor = if (text == "BREAK") White90 else Green90
+  }
+  updateColors()
+
+  val buttonFont = TextStyle(
+    fontFamily = sixtyFour,
+    fontWeight = FontWeight.Bold,
+    fontSize = 22.sp,
+    lineHeight = 0.sp,
+    letterSpacing = 2.sp
+  )
+
   Button(
     onClick = {
       timer.toggle()
-      buttonText = if (buttonText == "BREAK") "CONTINUE" else "BREAK"
-    }, modifier = modifier, shape = RoundedCornerShape(5.dp)
+      text = if (text == "BREAK") "CONTINUE" else "BREAK"
+      updateColors()
+    },
+    border = BorderStroke(
+      width = 3.dp,
+      color = borderColor
+    ),
+    colors = ButtonColors(backgroundColor, textColor, backgroundColor, textColor),
+    shape = RoundedCornerShape(5.dp)
   ) {
     Text(
-      buttonText, style = MaterialTheme.typography.labelLarge, modifier = modifier
+      text, style = buttonFont, modifier = modifier
     )
   }
 }
@@ -123,7 +154,20 @@ fun RenderBreakContinueButton(
   uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
 )
 @Composable
-fun PreviewContinueButton() {
+fun PreviewRenderedTimer() {
+  val timer = Timer(900)
+  RacingRegistersCompanionTheme {
+    Surface {
+      RenderedTimer(timer = timer, Modifier)
+    }
+  }
+}
+
+@Preview(
+  uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
+)
+@Composable
+fun PreviewRenderedContinueButton() {
   val timer = Timer(900)
   RacingRegistersCompanionTheme {
     Surface {
@@ -136,7 +180,7 @@ fun PreviewContinueButton() {
   uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
 )
 @Composable
-fun PreviewBreakButton() {
+fun PreviewRenderedBreakButton() {
   val timer = Timer(900)
   RacingRegistersCompanionTheme {
     Surface {
--- app/src/main/java/com/redpup/racingregisters/companion/ui/theme/Color.kt
+++ app/src/main/java/com/redpup/racingregisters/companion/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val White90 = Color(0xF0F0F0FF)
+val Green90 = Color(0xF07FFD77)
