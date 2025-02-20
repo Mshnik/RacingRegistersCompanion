134d90fe6f47bb1e91cc38cd3d18d28bbf463fbd
michaelpatashnik <mgpshnik@gmail.com>
2025-02-20 18:41:53 -0500

    Extract music params to xml, move music playing separate from volume

--- app/src/main/java/com/redpup/racingregisters/companion/sound/LoopMusic.kt
+++ app/src/main/java/com/redpup/racingregisters/companion/sound/LoopMusic.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import androidx.annotation.GuardedBy
 import com.redpup.racingregisters.companion.R
 
-/** Different tracks in the looped music. */
+/**
+ * Different tracks in the looped music.
+ *
+ * Enum order determines order tracks are added to the song.
+ */
 enum class Track {
   DRUMS_1,
   LEAD,
@@ -18,17 +22,25 @@ enum class Track {
  */
 class LoopMusic(context: Context) {
   private val tracks = mapOf(
-    Track.DRUMS_1 to LoopMediaPlayer(context, R.raw.music_drums),
+    Track.DRUMS_1 to LoopMediaPlayer(context, R.raw.music_drums_1),
     Track.DRUMS_2 to LoopMediaPlayer(context, R.raw.music_drums_2),
     Track.LEAD to LoopMediaPlayer(context, R.raw.music_lead),
     Track.PAD to LoopMediaPlayer(context, R.raw.music_pad),
     Track.BASS to LoopMediaPlayer(context, R.raw.music_bass)
   )
 
+  private val trackVolumes = mapOf(
+    Track.DRUMS_1 to context.resources.getFloat(R.dimen.music_volume_drums_1),
+    Track.DRUMS_2 to context.resources.getFloat(R.dimen.music_volume_drums_2),
+    Track.LEAD to context.resources.getFloat(R.dimen.music_volume_lead),
+    Track.PAD to context.resources.getFloat(R.dimen.music_volume_pad),
+    Track.BASS to context.resources.getFloat(R.dimen.music_volume_bass)
+  )
+
   private val trackLock = Object()
 
   @GuardedBy("trackLock")
-  private var trackVolumes: MutableMap<Track, Float> = mutableMapOf()
+  private var trackEnabled: MutableSet<Track> = mutableSetOf()
 
   var masterVolume: Float = 1.0F; set(value) {
     field = value
@@ -38,7 +50,6 @@ class LoopMusic(context: Context) {
   init {
     synchronized(trackLock) {
       Track.entries.forEach { t ->
-        trackVolumes[t] = 0.0F
         tracks[t]!!.setVolume(0.0F)
       }
     }
@@ -61,26 +72,18 @@ class LoopMusic(context: Context) {
   /** Returns true iff the given track is currently playing.  */
   fun isPlaying(track: Track): Boolean {
     synchronized(trackLock) {
-      return trackVolumes[track]!! > 0 && tracks[track]!!.isPlaying
-    }
-  }
-
-  /** Sets the volume of the given track. */
-  fun setVolume(track: Track, volume: Float) {
-    if (trackVolumes[track] != volume) {
-      synchronized(trackLock) {
-        if (trackVolumes[track] != volume) {
-          trackVolumes[track] = volume
-          updateVolume(track)
-        }
-      }
+      return trackEnabled.contains(track) && tracks[track]!!.isPlaying
     }
   }
 
   /** Updates track volume based on set volume args. */
   private fun updateVolume(track: Track) {
     synchronized(trackLock) {
-      tracks[track]!!.setVolume(trackVolumes[track]!! * masterVolume)
+      if (trackEnabled.contains(track)) {
+        tracks[track]!!.setVolume(trackVolumes[track]!! * masterVolume)
+      } else {
+        tracks[track]!!.setVolume(0.0F)
+      }
     }
   }
 
@@ -95,9 +98,8 @@ class LoopMusic(context: Context) {
   fun enableNextTrack() {
     synchronized(trackLock) {
       for (t in Track.entries) {
-        print("Track $t: $trackVolumes[t]")
-        if (trackVolumes[t] == 0.0f) {
-          trackVolumes[t] = 1.0f
+        if (!trackEnabled.contains(t)) {
+          trackEnabled.add(t)
           updateVolume(t)
           return
         }
