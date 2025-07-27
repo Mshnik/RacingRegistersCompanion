2cc71df75d322893db628c7cadd2e88019d91a0c
michaelpatashnik <mgpshnik@gmail.com>
2025-07-27 09:31:42 -0400

    Add remaining test cases for event bus

--- app/src/test/java/com/redpup/racingregisters/companion/event/EventBusTest.kt
+++ app/src/test/java/com/redpup/racingregisters/companion/event/EventBusTest.kt
@@ -29,10 +29,46 @@ class EventBusTest {
       eventBus.collectEvents { results.add(it) }
     }
 
-    eventBus.emit(1.tagged())
+    eventBus.emit(1)
+    eventBus.emit(1.tagged("tag"))
+    eventBus.emit(2)
     mainDispatcherRule.advanceUntilIdle()
 
-    assertThat(results).containsExactly(1.tagged())
+    assertThat(results).containsExactly(1.tagged(), 1.tagged("tag"), 2.tagged())
+    job.cancel()
+  }
+
+  @Test
+  fun subscribeSubscribesToFilteredEvents() = runBlocking {
+    val results = mutableListOf<Int>()
+    val job = launch(mainDispatcherRule.testDispatcher) {
+      eventBus.subscribe(1, 2, 3) { results.add(99) }
+    }
+
+    eventBus.emit(1)
+    eventBus.emit(2)
+    eventBus.emit(3)
+    eventBus.emit(4)
+    mainDispatcherRule.advanceUntilIdle()
+
+    assertThat(results).containsExactly(99, 99, 99)
+    job.cancel()
+  }
+
+  @Test
+  fun subscribeSubscribesToFilteredEvents_withTag() = runBlocking {
+    val results = mutableListOf<Int>()
+    val job = launch(mainDispatcherRule.testDispatcher) {
+      eventBus.subscribe(1, 2, 3, tag = "tag") { results.add(99) }
+    }
+
+    eventBus.emit(1.tagged("tag"))
+    eventBus.emit(2.tagged("tag"))
+    eventBus.emit(3.tagged("tag"))
+    eventBus.emit(3.tagged("other tag"))
+    mainDispatcherRule.advanceUntilIdle()
+
+    assertThat(results).containsExactly(99, 99, 99)
     job.cancel()
   }
 }
