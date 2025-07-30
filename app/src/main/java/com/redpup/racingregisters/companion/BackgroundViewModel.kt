305c8423083a1629ec1c54b3fc1d9679eee3c640
michaelpatashnik <mgpshnik@gmail.com>
2025-07-30 18:51:51 -0400

    Fix background accumulation

--- app/src/main/java/com/redpup/racingregisters/companion/BackgroundViewModel.kt
+++ app/src/main/java/com/redpup/racingregisters/companion/BackgroundViewModel.kt
@@ -1,11 +1,11 @@
 package com.redpup.racingregisters.companion
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.redpup.racingregisters.companion.timer.TimerViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.take
 
 /** A view model of the state of the background. */
 class BackgroundViewModel(val timer: TimerViewModel) : ViewModel() {
@@ -16,11 +16,9 @@ class BackgroundViewModel(val timer: TimerViewModel) : ViewModel() {
 
   /** Accumulates values from shift and shiftFactor into previousShift and previousTotal. */
   suspend fun accumulate() {
-    combine(shift, shiftFactor) { s, sF -> Pair(s, sF) }
-      .take(1)
-      .collect {
-        previousTotal.value += (it.first - previousShift.value) * it.second
-        previousShift.value = it.first
-      }
+    val s = shift.first()
+    val sF = shiftFactor.first()
+    previousTotal.value += (s - previousShift.value) * sF
+    previousShift.value = s
   }
 }
